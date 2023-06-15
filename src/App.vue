1bdd52cec426951de6ca4d3172ad8e93728d48c1
Eligio Biasi <ex_98@live.it>
2023-06-15 15:14:02 +0200

    error export App.Vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
-import HeaderApp from './components/HeaderApp.vue'
-import MainApp from './components/MainApp.vue'
-import FooterApp from './components/FooterApp.vue'
+import HeaderApp from './components/HeaderApp.vue';
+import MainApp from './components/MainApp.vue';
+import FooterApp from './components/FooterApp.vue';
+
+export default {
+    name: 'App',
+    components:{
+      HeaderApp,
+      MainApp,
+      FooterApp
+    }
+}
 </script>
 
 <template>
