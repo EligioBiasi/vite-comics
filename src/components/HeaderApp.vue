38cfaabe926095923f1fd4bffd26ccb84b029270
Eligio Biasi <ex_98@live.it>
2023-06-15 16:42:26 +0200

    added dinamicx links header

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,8 +1,17 @@
 <template >
     <header>
-        <div>
+        <div class="container">
             <nav>
-                <img src="../assets/img/dc-logo.png" alt="DC comics logo">
+                <div>
+                    <img src="../assets/img/dc-logo.png" alt="DC comics logo">
+                </div>
+                <div>
+                    <ul>
+                        <li v-for="link in NavContainerLinks">
+                            <a href="#">{{ link.text.toUpperCase() }}</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
     </header>
@@ -10,7 +19,45 @@
 
 <script>
 export default {
-    
+    name: 'HeaderApp',
+
+    data(){
+        return{
+            NavContainerLinks:[
+                {
+                    text: 'characters',
+                },
+                {
+                    text: 'comics',
+                },
+                {
+                    text: 'movies',
+                },
+                {
+                    text: 'tv',
+                },
+                {
+                    text: 'Games',
+                },
+                {
+                    text: 'collectibles',
+                },
+                {
+                    text: 'videos',
+                },
+                {
+                    text: 'fans',
+                },
+                {
+                    text: 'news',
+                },
+                {
+                    text: 'shop',
+                },
+                
+            ]
+        }
+    }
 }
 </script>
 
@@ -19,19 +66,42 @@ export default {
     @use '../style/partials/mixins' as *;
 
     header{
-        background-color: black;
+        background-color: $textBlack;
         padding-top: 2.5rem;
-
-        div{
-            background-color: white;
-        }
 
         nav{
             @include container;
+            @include flex(row,space-between);
             padding: 1rem;
+        }
 
+        div.container{
+            background-color: white;
             img{
                 width: 80px;
+            }
+        }
+
+        ul{
+            list-style-type: none;
+            @include flex();
+            
+            a{
+                text-decoration: none;
+                color: $textGray;
+                font-size: small;
+                font-weight: bold;
+            }
+
+            li{
+                @include flex();
+                padding: 0.45rem;
+                height: 100%;
+
+                :hover{
+                    color: #0282f9;
+                    border-bottom:#0282f9 solid 2px;
+                }
             }
         }
     }
