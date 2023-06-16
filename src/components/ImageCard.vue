44d232095a125c204e295f987572750c57979550
EligioBiasi <biasi.eligio@edufaimm.it>
2023-06-16 16:45:28 +0200

    attempt to add card

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,27 @@
+<template>
+     <article>
+        <div class="picture">
+            <img :src="ComicElement.thumb" alt="#">
+        </div>
+        <p class="cooking-time">
+            {{ComicElement.series}}
+        </p>
+    </article>
+</template>
+<script>
+
+export default {
+    props:{
+        ComicElement: String
+    }
+}
+</script>
+<style lang="scss" scoped>
+    article{
+        width: calc((100% / 6));
+
+        img{
+            width: 100%;
+        }
+    }
+</style>
